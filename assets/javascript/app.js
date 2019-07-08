b2e6760952fd1d369533a1ec4ca8813c1a6593f5
Dan Beccaria <beccdaniel@gmail.com>
2019-07-08 18:20:25 -0400

    testing img click functions and switching data in between

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
 //Loop through array to generate the number of buttons = to the number of items in the array
 //Create a function that takes in the users response and creates a new button based on the user input
 //Smallest Actionable Pieces (SAP)
-
+//data-
+//still-animate
+//click function grabs data state if state is still switches to animate value
 
 
 
@@ -14,6 +16,14 @@ $.ajax({
     method: "GET"
 }).then(function(response){
     console.log(response)
+    console.log(response.data[0].images.original)
+    //response.data[0].images.original_still.url
+    $('#image').append('<img src=' + response.data[0].images.original_still.url + '/>')
+    $('#image').on('click', function() {
+        $('#image').empty()
+        $('#image').append('<img src=' + response.data[0].images.original.url + '/>')
+        
+    })
 })
 
 
