a3c2cd68d0d45f7391723a549398e530ad8b1a87
Dan Beccaria <beccdaniel@gmail.com>
2019-07-09 21:28:53 -0400

    working on button that adds more buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,20 +7,40 @@ $(document).ready(function () {
 //still-animate
 //click function grabs data state if state is still switches to animate value
 
-let topics = ["nicolas cage", "sean connery", "werner herzog", "david lynch", "twin peaks", "dune", "trading places", "redlettermedia" ]
+let topics = ["nicolas cage","herzog", "david lynch", "mandy", "face/off", "redlettermedia", "mike stoklasa" ]
+//Adding styling using jquery instead of using css
+
+$('#header').append('<h1 id=title> Gif Generator </h1>').
+css("text-align", 'center').
+css('background-color', 'pink').
+css('width','500px').
+css('display','block').
+css('margin', '0 auto')
+$('#title').append('<p>Click on the buttons below to generate the images, click \'em again to view the gif version</p>')
 
 function getBtn() {
+    
+   
+    //------
     for(let i = 0; i < topics.length; i++) {
         console.log("loop works")
         let newBtn = topics[i];
         $("#buttons").append("<button id=newbtn" + i + ">" + newBtn + "</button>" )
         $("#newbtn"+ i).attr("data-name", topics[i])
+       
+    
         $("#newbtn" + i).on("click", function(){
-            $('#images').empty()
+            $('#img-container').empty()
             let stuff = $(this).attr("data-name")
             console.log(stuff)
             console.log(this)
             let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + stuff + "&limit=10&api_key=zTLXTIZUb2r2dOKO3slPidvtQ1hExHuU";
+        //----
+        
+         
+        
+        
+        
         $.ajax({
             url: queryURL, 
             method: "GET"
@@ -29,21 +49,23 @@ function getBtn() {
             let gifData = response.data;
            
           console.log(gifData)
+        //-------
+          
             for(let i = 0; i < gifData.length; i++) {
                 console.log("loop works") 
                 console.log(gifData[i].rating)
-                if(gifData.rating !== "pg-13" && gifData.rating !== "r"){
                 
+
+                $('#img-container').append('<div class=imghold id=imgholder_'+ i + '></div>')
+                $('#imgholder_'+ i).append('<div id=rating><p>Rating: ' + gifData[i].rating + ' </p></div>')
                 
-                $("#images").append("<img id=newimg" + i + " src="+ gifData[i].images.fixed_height_still.url + ">")
+                $("#imgholder_"+ i).append("<img id=newimg" + i + " src="+ gifData[i].images.fixed_height_still.url + ">")
                 $("#newimg" + i).attr("data-value", "still")
                 $("#newimg" + i).attr("data-still", gifData[i].images.fixed_height_still.url)
                 $("#newimg" + i).attr("data-animate", gifData[i].images.fixed_height.url)
-                $('#newimg' + i).prepend("<div id=rating>")
-                $('#rating').append('<p>Rating: ' + gifData[i].rating + '</p>')
+                
 
-
-            
+            //--------
                 $("#newimg" + i).on("click", function() {
 
 
@@ -62,15 +84,22 @@ function getBtn() {
                    
                 })
                 
-            }
+            
             }
             
         })
-        })
+    })
     }
+    $("#usersubmit").on('click', function(e){
+        event.preventDefault()
+        let userSearch = $('#usersearch').val()
+        $('#usersearch').val('')
+        topics.push(userSearch)
+        console.log(topics)
+    })
 }
 $('#clear').on('click', function() {
-    $('#images').empty()
+    $('#img-container').empty()
 })
 getBtn()
 
